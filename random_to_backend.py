645c912480133d833dcc1d2988f5021c29d00abd
Dávid Szomolai <davidszomolai1@gmail.com>
2025-05-06 20:22:31 +0200

    pridanie

--- random_to_backend.py
+++ random_to_backend.py
@@ -3,7 +3,8 @@ from time import sleep
 
 import requests
 
-url = "http://194.160.224.5:3000/message"
+#url = "http://194.160.224.5:3000/message"
+url = "http://localhost:3000/message"
 data_start = {
     "LON": random.randint(20, 25) * 100 / random.randrange(1, 5),
     "LAT": random.randint(20, 25) * 100 / random.randrange(1, 5),
@@ -12,7 +13,7 @@ data_start = {
     "AQI": random.randint(1, 5),
     "CO2": random.randint(1000, 50000),
     "PRESS": random.randint(1000, 500000),
-    "ALT_P": random.randint(1000, 50000),
+    "ALT_P": random.randint(1000, 50000)
 }
 while True:
     data = {
@@ -23,8 +24,8 @@ while True:
         "AQI": data_start["AQI"] + (random.randint(20, 25)/10 * random.randint(-1, 1)),
         "CO2": data_start["CO2"] + (random.randint(20, 25)/10 * random.randint(-1, 1)),
         "PRESS": data_start["PRESS"] + (random.randint(20, 25)/10 * random.randint(-1, 1)),
-        "ALT_P": data_start["ALT_P"] + (random.randint(20, 25)/10 * random.randint(-1, 1)),
+        "ALT_P": data_start["ALT_P"] + (random.randint(20, 25)/10 * random.randint(-1, 1))
     }
-    request = requests.post(url, data)
+    request = requests.post(url, json=data)
     print(data)
     sleep(2)
